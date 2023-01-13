d0e76d373a48bba1128ea4332dbf89b7fdcafdff
MBeklevic <m.beklevic37@gmail.com>
2023-01-13 21:56:44 +0300

    styles added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import ShoppingListForm from './Components/ShoppingListForm';
-import { v4 as uuidv4 } from 'uuid';
+import React, { Component } from "react";
+import ShoppingListForm from "./Components/ShoppingListForm";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -8,34 +9,32 @@ class App extends Component {
     this.state = {
       items: [
         { name: "Milk", qty: "2 gallons", id: uuidv4() },
-        { name: "Bread", qty: "2 loaves", id: uuidv4() }
-      ]
+        { name: "Bread", qty: "2 loaves", id: uuidv4() },
+      ],
     };
-    this.addItem = this.addItem.bind(this)
+    this.addItem = this.addItem.bind(this);
   }
 
   addItem(item) {
     let newItem = { ...item, id: uuidv4() };
-    this.setState(st => ({
-      items: [...st.items, newItem]
-    }))
+    this.setState((st) => ({
+      items: [...st.items, newItem],
+    }));
   }
 
   render() {
     return (
-      <>
-        <div className="App" >
-          <h1> Shopping List</h1>
-          <ul>
-            {this.state.items.map(item => (
-              <li key={item.id} id={item.id}>
-                {item.name} : {item.qty}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="App">
+        <h1> Shopping List</h1>
         <ShoppingListForm addItem={this.addItem} />
-      </>
+        <ul>
+          {this.state.items.map((item) => (
+            <li key={item.id} id={item.id}>
+              <span>{item.name}</span> : {item.qty}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
--- src/Components/ShoppingListForm.js
+++ src/Components/ShoppingListForm.js
@@ -1,47 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ShoppingListForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: "", qty: "" }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.addItem(this.state);
-        this.setState({ name: "", qty: "" })
-    }
+  constructor(props) {
+    super(props);
+    this.state = { name: "", qty: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addItem(this.state);
+    this.setState({ name: "", qty: "" });
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor='name'>Name:</label>
-                <input
-                    id='name'
-                    type="text"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor='qty'>Quantity</label>
-                <input
-                    id='qty'
-                    type="number"
-                    name="qty"
-                    value={this.state.qty}
-                    onChange={this.handleChange}
-                />
-                <button type='submit'>Submit</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="qty">Quantity:</label>
+        <input
+          id="qty"
+          type="number"
+          name="qty"
+          value={this.state.qty}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Add!</button>
+      </form>
+    );
+  }
 }
-
 
 export default ShoppingListForm;
